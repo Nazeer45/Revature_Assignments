014c4ae07b526c02f482b4554f6f1597b4fed4c4
Nazeer <nazeerazam45@gmail.com>
2024-03-06 12:56:05 +0530

    Day02 Prelunch Assignments

---
+++ Day02/PreLunch/Problem05A.java
@@ -0,0 +1,20 @@
+/*5. 
+Print the following patterns using loop :
+a.
+*
+**
+***
+****
+*/
+public class Problem05A {
+    public static void main(String[] args) {
+        int rows = 4;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
